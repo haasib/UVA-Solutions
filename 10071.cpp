efa68c5865c2a7747186fd7b272f349d224a6835
haasib <hasibulhasan873@gmail.com>
2016-11-02 00:45:20 +0600

    Create 10071.cpp

---
+++ 10071.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+    {
+        long long int a,b;
+        while(cin>>a>>b){
+            cout<<a*b*2<<endl;
+        }
+        return 0;
+    }
