5aa5ce3e76e20291d6fdf0e08c6665fcc4ebd5a7
haasib <hasibulhasan873@gmail.com>
2016-11-02 00:44:29 +0600

    Create 10055.cpp

---
+++ 10055.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+    {
+        long long int a,b;
+        while(cin>>a>>b){
+            if(a>b)
+                cout<<a-b<<endl;
+            else
+                cout<<b-a<<endl;
+        }
+        return 0;
+    }
