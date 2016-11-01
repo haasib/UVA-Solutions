5d1e6500fe0dd10483c046c3a8fcd087656f2084
haasib <hasibulhasan873@gmail.com>
2016-11-01 23:43:12 +0600

    Create 13140.cpp

---
+++ 13140.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+    {
+        cout<<"9999"<<" "<<99980001<<endl;
+        cout<<10000<<" "<<100000000<<endl;
+        cout<<10001<<" "<<100020001<<endl;
+        cout<<10002<<" "<<100040004<<endl;
+        cout<<10003<<" "<<100060009<<endl;
+        cout<<10004<<" "<<100080016<<endl;
+        cout<<10005<<" "<<100100025<<endl;
+        return 0;
+    }
