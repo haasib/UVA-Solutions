a8858bbf40f34c7191906557d00e004cf6379e0b
haasib <hasibulhasan873@gmail.com>
2016-11-01 23:38:33 +0600

    Create 11877.cpp

---
+++ 11877.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+    {
+        int n;
+        while(scanf("%d",&n) && n){
+            printf("%d\n",n/2);
+        }
+      return 0;
+    }
