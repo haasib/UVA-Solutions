e6d39f1ae08852664076eb5a6bd607ffe6c25156
MD. Hasibul Hasan <hasibulhasan873@gmail.com>
2016-11-02 19:47:16 +0600

    Create 12461.cpp

---
+++ 12461.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+    {
+        int n;
+        while(scanf("%d",&n) && n){
+            printf("1/2\n");
+        }
+        return 0;
+    }
