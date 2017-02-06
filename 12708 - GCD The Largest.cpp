7526c67bd31e0015f707529e62dcacdcb977fef4
MD. Hasibul Hasan <hasibulhasan873@gmail.com>
2017-02-06 22:05:51 +0600

    Create 12708 - GCD The Largest.cpp

---
+++ 12708 - GCD The Largest.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+    {
+        int tc;
+        ll n;
+        scanf("%d",&tc);
+        while(tc--){
+            cin>>n;
+            cout<<n/2<<endl;
+        }
+        return 0;
+    }
